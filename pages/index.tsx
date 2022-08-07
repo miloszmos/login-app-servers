0206db7c21768884be521d4940101fd6b68c0b68
miloszmos <milosz.moska@gmail.com>
2022-08-07 17:59:22 +0200

    linter(setup)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+
 const HomePage: NextPage = () => {
   return <h1 className="text-3xl">HomePage Page</h1>;
 };
